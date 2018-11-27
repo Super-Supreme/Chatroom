923de225814a77fb036f889c863c3497595fb87e
ZKai712 <44018070+ZKai712@users.noreply.github.com>
2018-11-27 16:51:34 +0800

    Add files via upload

---
+++ ChatRoom/ChatRoom/bin/com/dyxy/New_ChatRoom/src/com/dyxy/chat/Test01_Client.java
@@ -0,0 +1,121 @@
+package com.dyxy.chat;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Test01_Client {
+	public static void main(String[] args) {
+		
+		Socket client = null;
+		Send sender=null;
+		Receive receiver=null;
+		try {
+			client = new Socket("localhost", 9999);
+			System.out.println("已连接到服务器。。。");
+			Scanner input = new Scanner(System.in);
+			input.useDelimiter("\n");
+			sender=new Send(client);
+			receiver=new Receive(client);
+			
+			boolean isRunning=true;
+			while(isRunning) {
+				if(input.hasNext()) {
+					String inStr=input.next().trim();
+					sender.send_str(inStr);
+					if(inStr.equals("bye")) {
+						isRunning=false;
+					}
+					String isHas=null;
+					if((isHas=receiver.receive())!=null) {
+						System.out.println(isHas);
+					}
+				}
+			}
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			sender.close();
+			receiver.close();
+			try {
+				client.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+	
+	static class Receive{
+		DataInputStream in = null;
+		Socket client=null;
+		public Receive(Socket client) {
+			try {
+				this.client=client;
+				in = new DataInputStream(client.getInputStream());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		private String receive() {
+			String receive_str="";
+			try {
+				receive_str=in.readUTF();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return receive_str;
+		}
+		private void close() {
+			try {
+				in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				client.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	//发送
+	static class Send{
+		DataOutputStream out = null;
+		Socket client=null;
+		public Send(Socket client) {
+			this.client=client;
+			try {
+				out = new DataOutputStream(client.getOutputStream());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		private void send_str(String str) {
+			try {
+				out.writeUTF(str);
+				out.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		private void close() {
+			try {
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				client.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ ChatRoom/ChatRoom/bin/com/dyxy/New_ChatRoom/src/com/dyxy/chat03/ServerChat.java
@@ -0,0 +1,37 @@
+package com.dyxy.chat03;
+
+/*
+ * 服务器端
+ * 加入容器，实现群聊
+ * 
+ */
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ServerChat {
+	//添加并发容器CopyOnWriteArrayList<>()，存储所有client的信息
+	public static CopyOnWriteArrayList<ServerConnect> allClient=new CopyOnWriteArrayList<ServerConnect>();
+	public static void main(String[] args) {
+		ServerSocket server = null;
+		Socket client = null;
+		boolean isRunning = true;
+		int count=0;
+		try {
+			server = new ServerSocket(7777);
+			System.out.println("服务已启动，等待客户端连接。。。");
+			while (isRunning) {
+				client = server.accept();
+				System.out.println((++count)+"个客户端已连接");
+				ServerConnect sc=new ServerConnect(client);
+				//将每个client对象丢到容器中，进行管理。
+				allClient.add(sc);
+				new Thread(sc).start();
+			}
+		} catch (Exception e) {
+			System.out.println("服务器错误，请重新启动。。。");
+			isRunning = false;
+		}
+	}
+}
